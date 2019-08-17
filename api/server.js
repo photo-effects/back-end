632cd49ba9e47ce52ac271ba2126ecea2242d37a
Reyaad Rafeek <reyaad510@gmail.com>
2019-08-17 17:55:42 -0400

    Implemented cors and server.use(express.json());

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const server = express();
+server.use(express.json());
+server.use(cors());
+
 
 
 server.get('/', (req, res) => {
-    res.send('I am somewhere!!')
+    res.send('I am somewhere i thin!!')
 
 })
 
