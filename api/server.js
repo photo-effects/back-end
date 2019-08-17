9a5baeb31049414cd4b326a966597213d43fda5c
Reyaad Rafeek <reyaad510@gmail.com>
2019-08-17 17:42:46 -0400

    Added knex and cors dependencies

--- api/server.js
+++ api/server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const server = express();
-let pg = require('pg');
 
-let connect = "postgres://test:Rafeek123@localhost/test"
 
 server.get('/', (req, res) => {
- 
-    // res.send('I am alive!!')
+    res.send('I am somewhere!!')
 
 })
 
