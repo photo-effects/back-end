02bf462a1463ae3731c311f7e545a853360d555e
Reyaad Rafeek <reyaad510@gmail.com>
2019-08-30 16:09:40 -0400

    Testing using master endpoints and cloudinary master

--- database/seeds/02-projects.js
+++ database/seeds/02-projects.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     { title: 'photo2', user_id: '1', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1566871880/dgviedbtmf3qhgh2qcts.jpg', tags:'Memes', public_id:"dgviedbtmf3qhgh2qcts" },
     { title: 'photo3', user_id: '2', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1566872005/ywn0ky1ao0czyrl21azl.png', tags:'Funny' , public_id:"ywn0ky1ao0czyrl21azl"},
     { title: 'photo4', user_id: '2', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1566872179/hzbho5goxv6ez3fqhjce.jpg', tags:'Memes' , public_id:"hzbho5goxv6ez3fqhjce"},
-    { title: 'photo5', usuer_id: '1', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1567052338/nks3rwec8bfw0cnhqetw.jpg', tags: "Animals", public_id:"nks3rwec8bfw0cnhqetw" } 
+    { title: 'photo5', user_id: '1', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1567052338/nks3rwec8bfw0cnhqetw.jpg', tags: "Animals", public_id:"nks3rwec8bfw0cnhqetw" } 
    
   ])
 };
