05a2ebc299e917ee8f3263535b5859f524dea2f3
Reyaad Rafeek <reyaad510@gmail.com>
2019-08-30 17:43:56 -0400

    Changed a seed. Testing to make sure it works on heroku

--- database/seeds/02-projects.js
+++ database/seeds/02-projects.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   return knex('projects').insert([
-    { title: 'photo1', user_id: '1', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1566683148/f7cnfjzrnjiodlmganwz.jpg', tags:'Animals', public_id:"f7cnfjzrnjiodlmganwz"  },
+    { title: 'FF9:Vivi', user_id: '1', secure_url: 'https://res.cloudinary.com/dn94qw6w7/image/upload/v1567201170/ho0ji7zczgnxnfpgxxvp.jpg', tags:'Animals', public_id:"ho0ji7zczgnxnfpgxxvp"  },
     { title: 'photo2', user_id: '1', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1566871880/dgviedbtmf3qhgh2qcts.jpg', tags:'Memes', public_id:"dgviedbtmf3qhgh2qcts" },
     { title: 'photo3', user_id: '2', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1566872005/ywn0ky1ao0czyrl21azl.png', tags:'Funny' , public_id:"ywn0ky1ao0czyrl21azl"},
     { title: 'photo4', user_id: '2', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1566872179/hzbho5goxv6ez3fqhjce.jpg', tags:'Memes' , public_id:"hzbho5goxv6ez3fqhjce"},
