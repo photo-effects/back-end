22072a6d07053a48c4fd46441fba23d901e01413
Reyaad Rafeek <reyaad510@gmail.com>
2019-08-30 17:58:34 -0400

    Added new seeds to reflect real pics

--- database/seeds/02-projects.js
+++ database/seeds/02-projects.js
@@ -2,10 +2,10 @@
 exports.seed = function(knex, Promise) {
   return knex('projects').insert([
     { title: 'FF9:Vivi', user_id: '1', secure_url: 'https://res.cloudinary.com/dn94qw6w7/image/upload/v1567201170/ho0ji7zczgnxnfpgxxvp.jpg', tags:'Animals', public_id:"ho0ji7zczgnxnfpgxxvp"  },
-    { title: 'photo2', user_id: '1', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1566871880/dgviedbtmf3qhgh2qcts.jpg', tags:'Memes', public_id:"dgviedbtmf3qhgh2qcts" },
-    { title: 'photo3', user_id: '2', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1566872005/ywn0ky1ao0czyrl21azl.png', tags:'Funny' , public_id:"ywn0ky1ao0czyrl21azl"},
-    { title: 'photo4', user_id: '2', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1566872179/hzbho5goxv6ez3fqhjce.jpg', tags:'Memes' , public_id:"hzbho5goxv6ez3fqhjce"},
-    { title: 'photo5', user_id: '1', secure_url: 'https://res.cloudinary.com/cloudreyaad/image/upload/v1567052338/nks3rwec8bfw0cnhqetw.jpg', tags: "Animals", public_id:"nks3rwec8bfw0cnhqetw" } 
-   
+    { title: 'Architect', user_id: '1', secure_url: 'https://res.cloudinary.com/dn94qw6w7/image/upload/v1567201854/bzxzpo1fvggkbqaocvto.jpg', tags:'Memes', public_id:"bzxzpo1fvggkbqaocvto" },
+    { title: 'Day At The Park', user_id: '2', secure_url: 'https://res.cloudinary.com/dn94qw6w7/image/upload/v1567201953/py7kctx17kmaow0ecg0x.jpg', tags:'Funny' , public_id:"py7kctx17kmaow0ecg0x"},
+    { title: 'Beautiful Day', user_id: '2', secure_url: 'https://res.cloudinary.com/dn94qw6w7/image/upload/v1567202011/mnolikdrobnodqgpmoy5.jpg', tags:'Memes' , public_id:"mnolikdrobnodqgpmoy5"},
+    { title: 'I Love My Doggy!', user_id: '1', secure_url: 'https://res.cloudinary.com/dn94qw6w7/image/upload/v1567202073/rv8qvq2siyxqpbtwnl4i.jpg', tags: "Animals", public_id:"rv8qvq2siyxqpbtwnl4i" },
+    { title: 'Ballin!', user_id: '1', secure_url: 'https://res.cloudinary.com/dn94qw6w7/image/upload/v1567202141/mmqkomy3wcki6tm2tgo2.jpg', tags: "Animals", public_id:"mmqkomy3wcki6tm2tgo2" },
   ])
 };
