829faba6d3a2087ecd7336ac1fe599b2a98b9926
Reyaad Rafeek <reyaad510@gmail.com>
2019-10-04 12:41:46 -0400

    added async

--- api/server.js
+++ api/server.js
@@ -1,86 +1,77 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const cloudinary = require('cloudinary');
-const formData = require('express-form-data');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cloudinary = require("cloudinary");
+const formData = require("express-form-data");
 
-const usersRouter = require('../users/users-router.js');
-const projectsRouter = require('../projects/projects-router.js');
-const canvasprojectsRouter = require('../canvasprojects/canvasprojects-router.js');
+const usersRouter = require("../users/users-router.js");
+const projectsRouter = require("../projects/projects-router.js");
+const canvasprojectsRouter = require("../canvasprojects/canvasprojects-router.js");
 
 const server = express();
 
 // cloudinary config
-cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.API_KEY, 
-    api_secret: process.env.API_SECRET
-  })
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
 
 server.use(express.json());
 server.use(cors());
 server.use(formData.parse());
 
+server.use("/", usersRouter);
+server.use("/", projectsRouter);
+server.use("/canvas", canvasprojectsRouter);
 
-server.use('/', usersRouter);
-server.use('/', projectsRouter);
-server.use('/canvas', canvasprojectsRouter);
-
-server.get('/', (req, res) => {
-    res.send('Welcome to the Back-end for Photo Effects! :) Please enjoy your stay! Working try connecting! xoxo :)')
+server.get("/", (req, res) => {
+  res.send(
+    "Welcome to the Back-end for Photo Effects! :) Please enjoy your stay! Working try connecting! xoxo :)"
+  );
 });
 
 // After clicking "Choose File" this pushes image to cloudinary db
-server.post('/image-upload', (req, res) => {
-    const values = Object.values(req.files)
-    const promises = values.map(image => cloudinary.uploader.upload(image.path))
-    Promise
-        .all(promises)
-        .then(results => res.json(results))
-        .catch((err) => res.status(400).json(err));
-})
+server.post("/image-upload", (req, res) => {
+  const values = Object.values(req.files);
+  const promises = values.map(image => cloudinary.uploader.upload(image.path));
+  Promise.all(promises)
+    .then(results => res.json(results))
+    .catch(err => res.status(400).json(err));
+});
 
-server.post('/download', (req, res) => {
-    try {
-        await cloudinary.uploader.upload(req.body);
-        res.status(200).json({message: 'Went to cloudinary!'})
-    } catch(err) {
-        res.status(500).json({err: 'Something went wrong backend!'})
-    }
-})
+server.post("/download", async (req, res) => {
+  try {
+    await cloudinary.uploader.upload(req.body);
+    res.status(200).json({ message: "Went to cloudinary!" });
+  } catch (err) {
+    res.status(500).json({ err: "Something went wrong backend!" });
+  }
+});
 
+server.post("/cloudinary/upload", (req, res) => {
+  // const values = Object.values(req.files)
+  // const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  // Promise
+  //     .all(promises)
+  //     .then(results => res.json(results))
+  //     .catch((err) => res.status(400).json(err));
+  const imageURL = req.body;
+  cloudinary.uploader.upload(imageURL);
+  Promise.all(promises)
+    .then(results => res.json(results))
+    .catch(err => res.status(400).json(err));
+});
 
-
-server.post('/cloudinary/upload', (req, res) => {
-    // const values = Object.values(req.files)
-    // const promises = values.map(image => cloudinary.uploader.upload(image.path))
-    // Promise
-    //     .all(promises)
-    //     .then(results => res.json(results))
-    //     .catch((err) => res.status(400).json(err));
-    const imageURL = req.body;
-        cloudinary.uploader.upload(imageURL)
-        Promise
-        .all(promises)
-        .then(results => res.json(results))
-        .catch((err) => res.status(400).json(err));
-})
-
-
-server.delete('/image-delete', async (req, res) => {
-    try {
+server.delete("/image-delete", async (req, res) => {
+  try {
     await cloudinary.v2.uploader.destroy(req.body.public_id);
-    res.status(200).json({ message: `Image deleted` })
-    }
-    catch(err) {
-        res.status(500).json({
-            message: 'Error removing!'
-        })
-    }
-})
-
-
-
-
+    res.status(200).json({ message: `Image deleted` });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error removing!"
+    });
+  }
+});
 
 module.exports = server;
